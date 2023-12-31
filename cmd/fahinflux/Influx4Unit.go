b422196df105ec3aa194641dc1bef64aa8fb83d5
rolacher <rolacher@outlook.com>
2023-12-31 15:05:05 +0100

    Blind, shutter and awnning actuators added.

--- cmd/fahinflux/Influx4Unit.go
+++ cmd/fahinflux/Influx4Unit.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"time"
+
 	"github.com/guckykv/freeathome-go-fahapi/fahapi"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/influxdata/influxdb-client-go/v2/api/write"
-	"time"
 )
 
 func newInfluxPoint(unit fahapi.Unit) *write.Point {
@@ -21,6 +22,12 @@ func newInfluxPoint(unit fahapi.Unit) *write.Point {
 		return newInfluxPointRTC(fahapi.CastRTC(unit))
 	case fahapi.UntTypeWindowDoorSensor:
 		return newInfluxPointWDS(fahapi.CastWDS(unit))
+	case fahapi.UntTypeBlindActuator:
+		return newInfluxPointBAU(fahapi.CastBAU(unit))
+	case fahapi.UntTypeShutterActuator:
+		return newInfluxPointSHAU(fahapi.CastSHAU(unit))
+	case fahapi.UntTypeAwningActuator:
+		return newInfluxPointAAU(fahapi.CastAAU(unit))
 	}
 	return nil
 }
@@ -32,6 +39,58 @@ func newInfluxPoint(unit fahapi.Unit) *write.Point {
  * 2) In case of full update (we want that all all points are created at least every X minutes) we have
  *    to use the current time and not the LastUpdate (because there was no real update of the values)
  */
+
+func newInfluxPointBAU(bau *fahapi.BlindActuatorUnit) *write.Point {
+	tags := map[string]string{
+		"floor": bau.Floor,
+		"room":  bau.Room,
+	}
+	fields := map[string]interface{}{
+		"absolutePosBlinds": bau.AbsolutePosBlinds,
+	}
+	point := influxdb2.NewPoint(
+		"blinds",
+		tags,
+		fields,
+		time.Now(),
+	)
+	return point
+}
+
+func newInfluxPointSHAU(shau *fahapi.ShutterActuatorUnit) *write.Point {
+	tags := map[string]string{
+		"floor": shau.Floor,
+		"room":  shau.Room,
+	}
+	fields := map[string]interface{}{
+		"absolutePosBlinds": shau.AbsolutePosBlinds,
+		"absolutePosSlats": shau.AbsolutePosSlats,
+	}
+	point := influxdb2.NewPoint(
+		"blinds",
+		tags,
+		fields,
+		time.Now(),
+	)
+	return point
+}
+
+func newInfluxPointAAU(aau *fahapi.AwningActuatorUnit) *write.Point {
+	tags := map[string]string{
+		"floor": aau.Floor,
+		"room":  aau.Room,
+	}
+	fields := map[string]interface{}{
+		"absolutePosBlinds": aau.AbsolutePosBlinds,
+	}
+	point := influxdb2.NewPoint(
+		"blinds",
+		tags,
+		fields,
+		time.Now(),
+	)
+	return point
+}
 
 func newInfluxPointRTC(rtc *fahapi.RoomTemperatureControllerUnit) *write.Point {
 	tags := map[string]string{
